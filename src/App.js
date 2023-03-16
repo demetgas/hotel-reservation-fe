78c87f0959c878a58cd8b3f686ea3e6f816ee3d1
demetgas <demet_gas@hotmail.com>
2023-03-16 20:40:42 +0100

    error fixed

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import{
   Routes,
   Route,
 } from "react-router-dom";
+import Home from "./pages/home/Home";
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
-      
+      <Route path="/" element={<Home/>}/>
     </Routes>
     </BrowserRouter>
   );
