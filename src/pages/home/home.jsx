0304f0052d35384e0ddb07d416109989be74b35f
demetgas <demet_gas@hotmail.com>
2023-03-18 19:10:37 +0100

    creating navbar

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -2,8 +2,10 @@ import "./home.css";
 import Navbar from "../../components/Navbar";
 const Home = () => {
   return (
-    <div>
-      <Navbar />
+    <div className="navbar">
+      <div className="navContainer">
+        <span className="logo">Delta Booking</span>
+      </div>
     </div>
   );
 };
