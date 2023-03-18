59d960d1347bf886315bb36c5343955d945ed436
demetgas <demet_gas@hotmail.com>
2023-03-18 19:11:32 +0100

    Update home.jsx

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -5,6 +5,9 @@ const Home = () => {
     <div className="navbar">
       <div className="navContainer">
         <span className="logo">Delta Booking</span>
+        <div className="navItems">
+          <div className="navButton">Register</div>
+        </div>
       </div>
     </div>
   );
