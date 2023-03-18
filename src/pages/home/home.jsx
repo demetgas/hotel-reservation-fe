66811ac7a66560255568bd0b36a76638e6f4534a
demetgas <demet_gas@hotmail.com>
2023-03-18 19:37:16 +0100

    Update home.jsx

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div className="navbar">
       <div className="navContainer">
-        <span className="logo">Delta Booking</span>
+        <span className="logo">ELKOS-RM</span>
         <div className="navItems">
         <button className="navButton">Register</button>
         <button className="navButton">Login</button>
