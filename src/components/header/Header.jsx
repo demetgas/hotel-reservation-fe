04ed63af93fec99e26bd4db83a8f902ff11ce16e
demetgas <demet_gas@hotmail.com>
2023-03-19 17:00:31 +0100

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,17 @@
 import "./header.css";
-import {faCat} from "@fortawesome/free-solid-svg-icons"
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import { faCar, faCat, faPlane } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const Header = () => {
   return (
     <div className="header">
       <div className="headerList">
         <div className="headerListItem">
-        <FontAwesomeIcon icon={faCat} />
-        <span>Cat</span>
+          <FontAwesomeIcon icon={faCat} />
+          <span>Cat</span>
+          <FontAwesomeIcon icon={faPlane} />
+          <span>Flights</span>
+          <FontAwesomeIcon icon={faCar} />
+          <span>Car Rentals</span>
         </div>
       </div>
     </div>
