cab2e752392539260117da622884dd6f6529fdb2
demetgas <demet_gas@hotmail.com>
2023-03-19 17:10:56 +0100

    error fixed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,19 +4,23 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const Header = () => {
   return (
     <div className="header">
-      <div className="headerList">
-        <div className="headerListItem">
-          <div className="headerContainer">
-          <FontAwesomeIcon icon={faCat} />
-          <span>Cat</span>
-          <FontAwesomeIcon icon={faPlane} />
-          <span>Flights</span>
-          <FontAwesomeIcon icon={faCar} />
-          <span>Car Rentals</span>
+      <div className="headerContainer">
+        <div className="headerList">
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faCat} />
+            <span>Cat</span>
+          </div>
+            <div className="headerListItem">
+              <FontAwesomeIcon icon={faPlane} />
+              <span>Flights</span>
+            </div>
+            <div className="headerListItem">
+              <FontAwesomeIcon icon={faCar} />
+              <span>Car Rentals</span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
   );
 };
 
