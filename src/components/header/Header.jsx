9310d86439766f3f3980aa88f2aa9bde4fc84fac
demetgas <demet_gas@hotmail.com>
2023-03-19 16:57:58 +0100

    modules imported

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,14 @@
 import "./header.css";
-
+import {faCat} from "@fortawesome/free-solid-svg-icons"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 const Header = () => {
   return (
     <div className="header">
       <div className="headerList">
-        <div className="headerListItem"></div>
+        <div className="headerListItem">
+        <FontAwesomeIcon icon={faCat} />
+        <span>Cat</span>
+        </div>
       </div>
     </div>
   );
