dd56ead29df790a9ecdf7c0da3a5e2134b19d43b
demetgas <demet_gas@hotmail.com>
2023-04-25 20:15:08 +0300

    added a date library

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,7 +9,17 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { DateRange } from "react-date-range";
+import { useState } from "react";
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
 const Header = () => {
+  const [date, setDate] = useState([
+    {
+      startDate: new Date(),
+      endDate: null,
+      key: "selection",
+    },
+  ]);
   return (
     <div className="header">
       <div className="headerContainer">
@@ -55,6 +65,12 @@ const Header = () => {
           <div className="headerSearchItem">
             <FontAwesomeIcon icon={faCalendar} className="headerIcon" />
             <span className="headerSearchText">Dates</span>
+            <DateRange
+              editableDateInputs={true}
+              onChange={(item) => setDate([item.selection])}
+              moveRangeOnFirstSelection={false}
+              ranges={date}
+            />
           </div>
           <div className="headerSearchItem">
             <FontAwesomeIcon icon={faUserFriends} className="headerIcon" />
