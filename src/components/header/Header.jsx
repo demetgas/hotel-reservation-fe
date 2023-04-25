836150691e95a0b042bea52478941c21551e5a3d
demetgas <demet_gas@hotmail.com>
2023-04-25 20:59:29 +0300

    option items fixed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -104,13 +104,13 @@ const Header = () => {
                   <button className="optionCounterButton">+</button>
                 </div>
                 <div className="optionItem">
-                  <span className="optionText">Adult</span>
+                  <span className="optionText">Children</span>
                   <button className="optionCounterButton">-</button>
-                  <span className="optionCounterNumber">1</span>
+                  <span className="optionCounterNumber">0</span>
                   <button className="optionCounterButton">+</button>
                 </div>
                 <div className="optionItem">
-                  <span className="optionText">Adult</span>
+                  <span className="optionText">Room</span>
                   <button className="optionCounterButton">-</button>
                   <span className="optionCounterNumber">1</span>
                   <button className="optionCounterButton">+</button>
