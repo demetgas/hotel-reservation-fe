cb7a485cb16b8f47d9593538f5f2651d14602a7f
demetgas <demet_gas@hotmail.com>
2023-04-25 20:58:16 +0300

    created two more items

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -103,6 +103,18 @@ const Header = () => {
                   <span className="optionCounterNumber">1</span>
                   <button className="optionCounterButton">+</button>
                 </div>
+                <div className="optionItem">
+                  <span className="optionText">Adult</span>
+                  <button className="optionCounterButton">-</button>
+                  <span className="optionCounterNumber">1</span>
+                  <button className="optionCounterButton">+</button>
+                </div>
+                <div className="optionItem">
+                  <span className="optionText">Adult</span>
+                  <button className="optionCounterButton">-</button>
+                  <span className="optionCounterNumber">1</span>
+                  <button className="optionCounterButton">+</button>
+                </div>
               </div>
           </div>
           <div className="headerSearchItem">
