6b868e743508585ff20293b2db342d17de5415e9
demetgas <demet_gas@hotmail.com>
2023-05-02 17:33:36 +0300

    Update Featured.jsx

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -14,6 +14,39 @@ const Featured = () => {
           <h2>123</h2>
         </div>
       </div>
+      <div className="featuredItem">
+        <img
+          src="https://assets.traveltriangle.com/blog/wp-content/uploads/2018/05/busan.jpg"
+          alt=""
+          className="featuredImg"
+        />
+        <div className="featuredTitles">
+          <h1>Busan</h1>
+          <h2>123</h2>
+        </div>
+      </div>
+      <div className="featuredItem">
+        <img
+          src="https://live.staticflickr.com/4613/25485358037_902d7e3843_b.jpg"
+          alt=""
+          className="featuredImg"
+        />
+        <div className="featuredTitles">
+          <h1>Prizren</h1>
+          <h2>123</h2>
+        </div>
+      </div>
+      <div className="featuredItem">
+        <img
+          src="https://live.staticflickr.com/4613/25485358037_902d7e3843_b.jpg"
+          alt=""
+          className="featuredImg"
+        />
+        <div className="featuredTitles">
+          <h1>Prizren</h1>
+          <h2>123</h2>
+        </div>
+      </div>
     </div>
   );
 };
