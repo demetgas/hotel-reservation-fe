69fd820892124792b86c41bee9a2f912c118f883
demetgas <demet_gas@hotmail.com>
2023-05-02 21:51:12 +0300

    Update FeaturedProps.jsx

--- src/components/featuredProps/FeaturedProps.jsx
+++ src/components/featuredProps/FeaturedProps.jsx
@@ -1,7 +1,14 @@
 import "./featuredProps.css";
 
 const FeaturedProps = () => {
-  return <div>FeaturedProps</div>;
+  return (
+    <div className="fProps">
+        <img src="" alt="" className="fPropsImg" />
+        <span className="fPropsName">Apart hotel Stare Miasto</span>
+        <span className="fPropsCity">Tokyo</span>
+        <span className="fPropsPrice">Starting from $12</span>
+    </div>
+  )
 };
 
 export default FeaturedProps;
