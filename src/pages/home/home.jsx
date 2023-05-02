1a97018d1ed1a305603827620aa7afeeac531a8e
demetgas <demet_gas@hotmail.com>
2023-05-02 20:58:11 +0300

    Update home.jsx

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
       <Header />
       <div className="homeContainer">
       <Featured />
-      <h1 className="homeTitle">browse by property type</h1>
+      <h1 className="homeTitle">Browse by property type</h1>
       <PropertyList/>
       </div>
     </div>
