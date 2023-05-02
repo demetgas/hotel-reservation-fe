9f50594e446d2bd032a32e1195581c3d6142df56
demetgas <demet_gas@hotmail.com>
2023-05-02 21:51:54 +0300

    Update FeaturedProps.jsx

--- src/components/featuredProps/FeaturedProps.jsx
+++ src/components/featuredProps/FeaturedProps.jsx
@@ -6,7 +6,10 @@ const FeaturedProps = () => {
         <img src="" alt="" className="fPropsImg" />
         <span className="fPropsName">Apart hotel Stare Miasto</span>
         <span className="fPropsCity">Tokyo</span>
-        <span className="fPropsPrice">Starting from $12</span>
+        <span className="fPropsPrice">Starting from $120</span>
+        <div className="fPropsRating">
+            <button></button>
+        </div>
     </div>
   )
 };
