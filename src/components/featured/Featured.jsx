5105e8d95ceed4a0b25ed564b44c0aa664e8b2d9
demetgas <demet_gas@hotmail.com>
2023-05-02 17:25:08 +0300

    Update Featured.jsx

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -4,7 +4,8 @@ const Featured = () => {
   return (
     <div className="featured">
       <div className="featuredItem">
-        <img src="" alt="" className="featuredImg" />
+        <img src="https://live.staticflickr.com/4613/25485358037_902d7e3843_b.jpg" alt="" className="featuredImg" />
+        <h1>Kosova</h1>
         <div className="featuredTitles"></div>
       </div>
     </div>
