f98deffedb8caffcfd025108475119793fe5c670
demetgas <demet_gas@hotmail.com>
2023-05-02 22:54:44 +0300

    mail updated and used in home

--- src/components/mailList/MailList.jsx
+++ src/components/mailList/MailList.jsx
@@ -1,9 +1,16 @@
-import "./mailList.css"
+import "./mailList.css";
 
 const MailList = () => {
   return (
-    <div>MailList</div>
-  )
-}
+    <div className="mail">
+      <h1 className="mailTitle">Save time and money</h1>
+      <span className="mailDesc">Sign up for information</span>
+      <div className="mailInputContainer">
+        <input type="text" placeholder="Your Email" />
+        <button>Subscribe</button>
+      </div>
+    </div>
+  );
+};
 
-export default MailList
+export default MailList;
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,6 +1,7 @@
 import Featured from "../../components/featured/Featured";
 import FeaturedProps from "../../components/featuredProps/FeaturedProps";
 import Header from "../../components/header/Header";
+import MailList from "../../components/mailList/MailList";
 import Navbar from "../../components/navbar/Navbar";
 import PropertyList from "../../components/propertyList/PropertyList";
 import "./home.css";
@@ -15,6 +16,7 @@ const Home = () => {
         <PropertyList />
         <h1 className="homeTitle">Most preferred hotels</h1>
         <FeaturedProps />
+        <MailList/>
       </div>
     </div>
   );
