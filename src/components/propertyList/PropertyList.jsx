5326ebb049b5ee6557aaa5ba830090b9cb826f7e
demetgas <demet_gas@hotmail.com>
2023-05-02 20:35:31 +0300

    Update PropertyList.jsx

--- src/components/propertyList/PropertyList.jsx
+++ src/components/propertyList/PropertyList.jsx
@@ -1,7 +1,16 @@
 import "./PropertyList.css";
 
 const PropertyList = () => {
-  return <div>PropertyList</div>;
+  return(
+   <div className="pList">
+    <div className="pListItem">
+        <img src="" alt="" className="pListImg" />
+        <div className="pListTitles">
+            <h1>Hello</h1>
+        </div>
+    </div>
+   </div> 
+  ) 
 };
 
 export default PropertyList;
