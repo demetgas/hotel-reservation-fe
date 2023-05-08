ae23ac4010a48f4f09d429afb2d6c88a57e5bdae
demetgas <demet_gas@hotmail.com>
2023-05-08 14:29:29 +0300

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -44,7 +44,7 @@ const Footer = () => {
           <li className="fListItem">Terms & conditions</li>
         </ul>
       </div>
-      <div className="fText">Copyright © 2023 ELKOS/RM. </div>
+      <div className="fText">Copyright © 2023 ELKOS/RM -mo. </div>
     </div>
   );
 };
