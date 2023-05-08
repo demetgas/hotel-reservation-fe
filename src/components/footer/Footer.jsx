687ebd404086c2ca4a4384112485b4f55aa73c42
demetgas <demet_gas@hotmail.com>
2023-05-08 13:53:29 +0300

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,20 @@
 import "./footer.css";
 
 const Footer = () => {
-    return   (  <div className="footer">Footer</div> )
-  
-}
+  return (
+    <div className="footer">
+      <div className="fLists">
+        <ul className="fList">
+          <li className="fListItem">Countries</li>
+          <li className="fListItem">Regions</li>
+          <li className="fListItem">Cities</li>
+          <li className="fListItem">Districts</li>
+          <li className="fListItem">Airports</li>
+          <li className="fListItem">Hotels</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
