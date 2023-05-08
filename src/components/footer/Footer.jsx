c7e0adc366890e7447bca63face4faf7edecfac4
demetgas <demet_gas@hotmail.com>
2023-05-08 13:54:14 +0300

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -12,6 +12,38 @@ const Footer = () => {
           <li className="fListItem">Airports</li>
           <li className="fListItem">Hotels</li>
         </ul>
+        <ul className="fList">
+          <li className="fListItem">Countries</li>
+          <li className="fListItem">Regions</li>
+          <li className="fListItem">Cities</li>
+          <li className="fListItem">Districts</li>
+          <li className="fListItem">Airports</li>
+          <li className="fListItem">Hotels</li>
+        </ul>
+        <ul className="fList">
+          <li className="fListItem">Countries</li>
+          <li className="fListItem">Regions</li>
+          <li className="fListItem">Cities</li>
+          <li className="fListItem">Districts</li>
+          <li className="fListItem">Airports</li>
+          <li className="fListItem">Hotels</li>
+        </ul>
+        <ul className="fList">
+          <li className="fListItem">Countries</li>
+          <li className="fListItem">Regions</li>
+          <li className="fListItem">Cities</li>
+          <li className="fListItem">Districts</li>
+          <li className="fListItem">Airports</li>
+          <li className="fListItem">Hotels</li>
+        </ul>
+        <ul className="fList">
+          <li className="fListItem">Countries</li>
+          <li className="fListItem">Regions</li>
+          <li className="fListItem">Cities</li>
+          <li className="fListItem">Districts</li>
+          <li className="fListItem">Airports</li>
+          <li className="fListItem">Hotels</li>
+        </ul>
       </div>
     </div>
   );
