122e003553dfc55cd4518f78352064f8a9b1b7eb
demetgas <demet_gas@hotmail.com>
2023-05-09 21:50:18 +0300

    updated hotel

--- src/pages/hotel/Hotel.jsx
+++ src/pages/hotel/Hotel.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import "./hotel.css";
+import Navbar from "../../components/navbar/Navbar";
+import Header from "../../components/header/Header";
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Hotel() {
-  return <div>Hotel</div>;
+  return (
+    <div>
+      <Navbar />
+      <Header type="list" />
+      <div className="hotelContainer">
+        <div className="hotelWrapper">
+          <h1 className="hotelTitle">Grand Hotel</h1>
+          <div className="hotelAddress">
+            <FontAwesomeIcon icon={faLocationDot} />
+            <span>New York 123</span>
+          </div>
+          <span className="hotelDistance">Great location</span>
+          <span className="hotelPriceHighlight">
+            Cheapest hotel in New York
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Hotel;
