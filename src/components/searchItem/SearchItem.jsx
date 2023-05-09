a8a111158e1de6a192dff5f2384b63592d606010
demetgas <demet_gas@hotmail.com>
2023-05-09 17:28:29 +0300

    added descriptions for a hotel

--- src/components/searchItem/SearchItem.jsx
+++ src/components/searchItem/SearchItem.jsx
@@ -8,7 +8,17 @@ const SearchItem = () => {
         alt=""
         className="siImg"
       />
-      <div className="siDesc"></div>
+      <div className="siDesc">
+        <h1 className="siTitle"> Courtesy The Greenwich</h1>
+        <span className="siDistance">5 mins from central park</span>
+        <span className="siTaxiOp">Free airport taxi</span>
+        <span className="siSub">Greatest hotel in New York</span>
+        <span className="siFeatures">1 bathroom - 2 rooms</span>
+        <span className="siCancelOp">Free cancellation</span>
+        <div className="siCancelOpSub">
+          Get this price, cancel later if you want.
+        </div>
+      </div>
       <div className="siDetails"></div>
     </div>
   );
