8fabc687a69b36d7c64c6d7e84dd5b7e45203596
demetgas <demet_gas@hotmail.com>
2023-05-09 17:20:54 +0300

    Update SearchItem.jsx

--- src/components/searchItem/SearchItem.jsx
+++ src/components/searchItem/SearchItem.jsx
@@ -1,11 +1,17 @@
 import "./searchItem.css";
 
 const SearchItem = () => {
-  return(
-     <div className="searchItem">
-      <img src="https://media.cntraveler.com/photos/61d2c535d34478cd18de0ded/16:9/w_2560,c_limit/Greenwich-Hotel-Lobby-CourtesyHotel.jpeg" alt="" className="siImg"/>
-     </div>
-  )
+  return (
+    <div className="searchItem">
+      <img
+        src="https://media.cntraveler.com/photos/61d2c535d34478cd18de0ded/16:9/w_2560,c_limit/Greenwich-Hotel-Lobby-CourtesyHotel.jpeg"
+        alt=""
+        className="siImg"
+      />
+      <div className="siDesc"></div>
+      <div className="siDetails"></div>
+    </div>
+  );
 };
 
 export default SearchItem;
