4938bf9be6de455a53457be42018330203bfff9e
demetgas <demet_gas@hotmail.com>
2023-05-19 16:36:11 +0200

    deetails added

--- src/pages/hotel/Hotel.jsx
+++ src/pages/hotel/Hotel.jsx
@@ -48,6 +48,28 @@ const Hotel = () => {
               </div>
             ))}
           </div>
+          <div className="hotelDetails">
+            <div className="hotelDetailsTexts">
+              <h1 className="hotelTitle">Coolest hotel in New York</h1>
+              <p className="hotelDesc">
+                Situated in the upscale SoHo neighborhood, this luxury hotel is
+                close to art galleries, designer shops and popular restaurants.
+                This modern hotel features an outdoor restaurant and all rooms
+                have city views. The elegant guest rooms at the Hotel Soho Grand
+                come with free Wi-Fi, a flat-screen TV and a clcok radio.
+                Designer bathrobes and toiletries are also provided. Guests can
+                enjoy American cuisine and signature cocktails in the Club Room,
+                the Grand Bar and Lounge. Opening seasonally, Gilligans is an
+                outdoor restaurant offering a fresh summer menu. Soho Grand
+                Hotel offers a 24-hour gym and business center. A helpful
+                concierge desk is also available 24-hours. Canal Street Subway
+                Station is a 5-minute walk from Soho Grand and provides easy
+                access to Central Park and Times Square. Little Italy is a
+                10-minute walk away. This is our guests' favourite part of New
+                York, according to independent reviews.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
