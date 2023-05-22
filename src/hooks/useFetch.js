0024e84d0797f40c44ef91a6b0a00a60874d8b64
demetgas <demet_gas@hotmail.com>
2023-05-22 18:58:18 +0200

    Update useFetch.js

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,8 +1,13 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const useFetch = (url) => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState([false]);
   const [error, setError] = useState([false]);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+    };
+  });
 };
