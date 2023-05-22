c8e5997d4548de96c5f11a0c49a6232a92bad5f8
demetgas <demet_gas@hotmail.com>
2023-05-22 20:49:02 +0200

    Update PropertyList.jsx

--- src/components/propertyList/PropertyList.jsx
+++ src/components/propertyList/PropertyList.jsx
@@ -19,16 +19,14 @@ const PropertyList = () => {
       ) : (
         <>
           {data &&
-            images.map((img,i) => (
+            images.map((img, i) => (
               <div className="pListItem">
-                <img
-                  src={img}
-                  alt=""
-                  className="pListImg"
-                />
+                <img src={img} alt="" className="pListImg" />
                 <div className="pListTitles">
-                  <h1>Hotels</h1>
-                  <h2>{data[i]}</h2>
+                  <h1>{data[i].type}</h1>
+                  <h2>
+                    {data[i].count} {data[i].type}
+                  </h2>
                 </div>
               </div>
             ))}
