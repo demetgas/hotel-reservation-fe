70b7eeacd1f242e276dee72111e32716fe3fbe77
demetgas <demet_gas@hotmail.com>
2023-05-22 19:28:52 +0200

    added if else condition

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -2,41 +2,48 @@ import useFetch from "../../hooks/useFetch";
 import "./featured.css";
 
 const Featured = () => {
-
-  const { data, loading, error } = useFetch("/hotels/countByCity?cities=ankara,london,seoul")
+  const { data, loading, error } = useFetch(
+    "/hotels/countByCity?cities=ankara,london,seoul"
+  );
   return (
     <div className="featured">
-      {<><div className="featuredItem">
-        <img
-          src="https://www.goldentours.com/travelblog/wp-content/uploads/2015/07/shutterstock_555823393-973x649.jpg"
-          alt=""
-          className="featuredImg"
-        />
-        <div className="featuredTitles">
-          <h1>London</h1>
-        </div>
-      </div>
-      
-      <div className="featuredItem">
-        <img
-          src="https://koreankulture.com/wp-content/uploads/Gyeongbokgung-Palace-in-Seoul-South-Korea.jpg"
-          alt=""
-          className="featuredImg"
-        />
-        <div className="featuredTitles">
-          <h1>Seoul</h1>
-        </div>
-      </div>
-      <div className="featuredItem">
-        <img
-          src="https://media.tacdn.com/media/attractions-splice-spp-674x446/0b/17/39/75.jpg"
-          alt=""
-          className="featuredImg"
-        />
-        <div className="featuredTitles">
-          <h1>Lapland</h1>
-        </div>
-      </div></>}
+      {loading ? (
+        "It is loading"
+      ) : (
+        <>
+          <div className="featuredItem">
+            <img
+              src="https://www.goldentours.com/travelblog/wp-content/uploads/2015/07/shutterstock_555823393-973x649.jpg"
+              alt=""
+              className="featuredImg"
+            />
+            <div className="featuredTitles">
+              <h1>London</h1>
+            </div>
+          </div>
+
+          <div className="featuredItem">
+            <img
+              src="https://koreankulture.com/wp-content/uploads/Gyeongbokgung-Palace-in-Seoul-South-Korea.jpg"
+              alt=""
+              className="featuredImg"
+            />
+            <div className="featuredTitles">
+              <h1>Seoul</h1>
+            </div>
+          </div>
+          <div className="featuredItem">
+            <img
+              src="https://media.tacdn.com/media/attractions-splice-spp-674x446/0b/17/39/75.jpg"
+              alt=""
+              className="featuredImg"
+            />
+            <div className="featuredTitles">
+              <h1>Lapland</h1>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
