35e2f1b2c770648a0ccc68471871cfdbea8be394
demetgas <demet_gas@hotmail.com>
2023-05-23 18:53:36 +0200

    Update SearchItem.jsx

--- src/components/searchItem/SearchItem.jsx
+++ src/components/searchItem/SearchItem.jsx
@@ -4,11 +4,11 @@ const SearchItem = ({ item }) => {
   return (
     <div className="searchItem">
       <div className="siDesc">
-        <h1 className="siTitle"> Courtesy The Greenwich</h1>
-        <span className="siDistance">5 mins from central park</span>
+        <h1 className="siTitle"> {item.name}</h1>
+        <span className="siDistance">{item.distancefromcenter}m from center </span>
         <span className="siTaxiOp">Free airport taxi</span>
         <span className="siSub">Greatest hotel in New York</span>
-        <span className="siFeatures">1 bathroom - 2 rooms</span>
+        <span className="siFeatures">{item.description}</span>
         <span className="siCancelOp">Free cancellation</span>
         <div className="siCancelOpSub">
           Get this price, cancel later if you want.
