1e52b9436bb791b01b31c75f1836bf6358d1f814
demetgas <demet_gas@hotmail.com>
2023-05-23 21:59:40 +0200

    Update List.jsx

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -18,7 +18,7 @@ const List = () => {
   const [max, setMax] = useState(undefined);
 
   const { data, loading, error, reFetch } = useFetch(
-    `/hotels?cityname=${destination}&min=${min}&max=${max}`
+    `/hotels?cityname=${destination}&min=${min || 0}&max=${max || 999}`
   );
 
   const handleClick = () => {
