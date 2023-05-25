76037cd10514bfab0465a9584d726bcbe62b4405
demetgas <demet_gas@hotmail.com>
2023-05-25 18:10:36 +0200

    wrapped app with ConfrimContextProvider

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { SearchContextProvider } from "./context/SearchContext";
+import { ConfrimContextProvider } from "./context/ConfirmContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <SearchContextProvider>
-      <App />
-    </SearchContextProvider>
+    <ConfrimContextProvider>
+      <SearchContextProvider>
+        <App />
+      </SearchContextProvider>
+    </ConfrimContextProvider>
   </React.StrictMode>
 );
