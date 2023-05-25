983f8ba2f46a6686c99ead9997464db6eae24fe8
demetgas <demet_gas@hotmail.com>
2023-05-25 15:47:52 +0200

    declared SearchReducer

--- src/context/SearchContext.js
+++ src/context/SearchContext.js
@@ -11,3 +11,14 @@ const INITIAL_STATE = {
 };
 
 export const SearchContext = createContext(INITIAL_STATE);
+
+const SearchReducer = (state, action) => {
+  switch (action.type) {
+    case "NEW_REQ":
+      return action.playload;
+    case "RESET_REQ":
+      return INITIAL_STATE;
+    default:
+      return state;
+  }
+};
