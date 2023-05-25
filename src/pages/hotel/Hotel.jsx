af81b62743441759d5aafe0b8583f84a79d4711e
demetgas <demet_gas@hotmail.com>
2023-05-25 16:33:09 +0200

    error fixed

--- src/pages/hotel/Hotel.jsx
+++ src/pages/hotel/Hotel.jsx
@@ -14,6 +14,7 @@ import { useContext, useState } from "react";
 import { useLocation } from "react-router-dom";
 import useFetch from "../../hooks/useFetch";
 import { SearchContext } from "../../context/SearchContext";
+
 const Hotel = () => {
   const location = useLocation();
   const id = location.pathname.split("/")[2];
