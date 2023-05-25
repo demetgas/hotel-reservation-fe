b91ab93fe8878b58c26d3821b54814a0392bc7df
demetgas <demet_gas@hotmail.com>
2023-05-25 19:08:47 +0200

    credentials and setC created

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
 import "./login.css";
 
 const Login = () => {
+  const [credentials, setCredentials] = useState({
+    username: undefined,
+    password: undefined,
+  });
   return <div>Login</div>;
 };
 
