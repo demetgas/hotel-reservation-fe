1029b4c87e8fbfa5b398f1e38bbae5d5b55999cf
demetgas <demet_gas@hotmail.com>
2023-05-25 17:17:35 +0200

    copied searchContext to create ConfirmContext

---
+++ src/context/ConfirmContext.js
@@ -0,0 +1,41 @@
+import { createContext, useReducer } from "react";
+
+const INITIAL_STATE = {
+  city: undefined,
+  date: [],
+  options: {
+    adult: undefined,
+    children: undefined,
+    room: undefined,
+  },
+};
+
+export const ConfrimContext = createContext(INITIAL_STATE);
+
+const ConfrimReducer = (state, action) => {
+  switch (action.type) {
+    case "NEW_REQ":
+      return action.payload;
+    case "RESET_REQ":
+      return INITIAL_STATE;
+    default:
+      return state;
+  }
+};
+
+export const ConfrimContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(ConfrimReducer, INITIAL_STATE);
+
+  return (
+    <ConfrimContext.Provider
+      value={{
+        city: state.city,
+        date: state.date,
+        options: state.options,
+        dispatch,
+      }}
+    >
+      {children}
+    </ConfrimContext.Provider>
+  );
+};
