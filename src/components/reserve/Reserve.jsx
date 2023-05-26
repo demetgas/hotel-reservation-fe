a6774924f694bc5aecb70d9c45cba85d3fbd823c
demetgas <demet_gas@hotmail.com>
2023-05-26 22:10:18 +0200

    Update Reserve.jsx

--- src/components/reserve/Reserve.jsx
+++ src/components/reserve/Reserve.jsx
@@ -1,7 +1,14 @@
 import "./reserve.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Reserve = (setOpen, hotelId) => {
-  return <div className="reserve">Reserve</div>;
+  return (
+    <div className="reserve">
+      <div className="rContainer">
+        <FontAwesomeIcon />
+      </div>
+    </div>
+  );
 };
 
 export default Reserve;
