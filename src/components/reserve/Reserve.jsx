e6d509ab6b804074e01c9be5f126256bfe669332
demetgas <demet_gas@hotmail.com>
2023-05-26 22:11:56 +0200

    Update Reserve.jsx

--- src/components/reserve/Reserve.jsx
+++ src/components/reserve/Reserve.jsx
@@ -1,11 +1,12 @@
 import "./reserve.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimesCircle } from "@fortawesome/free-solid-svg-icons";
 
 const Reserve = (setOpen, hotelId) => {
   return (
     <div className="reserve">
       <div className="rContainer">
-        <FontAwesomeIcon />
+        <FontAwesomeIcon icon={faTimesCircle} />
       </div>
     </div>
   );
