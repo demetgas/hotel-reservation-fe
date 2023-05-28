ebe41c872c9d5eb03c31c175bc0d5858a65fe8ae
demetgas <demet_gas@hotmail.com>
2023-05-28 12:48:58 +0200

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,31 +1,39 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./navbar.css";
 import { useContext, useState } from "react";
 import { ConfirmContext } from "../../context/ConfirmContext";
 
+
 const Navbar = () => {
   const { user } = useContext(ConfirmContext);
-
-  const handleClick = () =>{
-    
-  }
-
+  const { navigate } = useNavigate();
+ 
   return (
     <div className="navbar">
       <div className="navContainer">
         <Link className="link" to="/">
           <span className="logo">ELKOS/RM</span>
         </Link>
-       {user ? user.username : <div className="navItems">
-          <button className="navButton">Register</button>
-          <button className="navButton" >Login</button>
-        </div>}
-        <div className="navItems">
-          <button className="navButton" onClick={handleClick}>Log Out</button>
-        </div>
+        {user ? (
+          user.username
+        ) : (
+          <div className="navItems">
+            <button className="navButton">Register</button>
+            <button className="navButton">Login</button>
+          </div>
+        )}
+        {!user ? null : (
+          <div className="navItemss">
+            <button className="navButton" onClick={(e)=>{
+              localStorage.remove(user.username) 
+            }}>
+              Log Out
+            </button>
+          </div>
+        )}
       </div>
     </div>
-  ); 
+  );
 };
 
 export default Navbar;
