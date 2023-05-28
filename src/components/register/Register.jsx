8b6bcef07ea273d31f03b158508c7fbda544d892
demetgas <demet_gas@hotmail.com>
2023-05-28 11:52:03 +0200

    Update Register.jsx

--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -1,6 +1,12 @@
+import { useState } from "react";
 import "./register.css";
 
 const Register = () => {
+  const [credentials, setCredentials] = useState({
+    username: undefined,
+    email: undefined,
+    pwd: undefined,
+  });
   return <div>Register</div>;
 };
 
